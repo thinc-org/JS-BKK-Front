f68f9963cbcc68fabbb955c5091261ec811fc431
new5558 <new17353@gmail.com>
2019-12-23 03:00:13 +0700

    feat: login page

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,17 +1,78 @@
-import React, { useCallback, useContext } from 'react';
+import React, { useCallback, useContext, useState, useMemo } from 'react';
 import { observer } from 'mobx-react-lite';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
 import { rootContext } from '../_app';
+
+interface ErrorProps {
+  error: string | null;
+}
 
 const Home: React.FC = observer(() => {
   const { userStore } = useContext(rootContext);
-  const authenticate = useCallback(() => userStore.setToken('validtoken'), []);
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  const login = useCallback(
+    e => {
+      e.preventDefault();
+      if (username === 'admin' && password === '123456') {
+        userStore.setToken('validtoken');
+        router.push('/user/profile');
+      } else {
+        setError('Username or password wrong');
+        setPassword('');
+        setUsername('');
+      }
+    },
+    [username, password]
+  );
+
+  const Input = useMemo(
+    () => styled.input`
+      padding: 10px;
+    `,
+    []
+  );
+
+  const Container = useMemo(
+    () => styled.div`
+      display: flex;
+      flex-direction: column;
+    `,
+    []
+  );
+
+  const Error = useMemo(
+    () => styled.span<ErrorProps>`
+      color: red;
+      font-size: 20px;
+      display: ${props => (props.error ? 'block' : 'none')};
+    `,
+    []
+  );
+
   return (
     <div>
-      <div> Login Works</div>
-      <div> userStore: {userStore.userInfo && userStore.userInfo.name}</div>
-      <button type='button' onClick={authenticate}>
-        Login
-      </button>
+      <div>Login</div>
+      <form onSubmit={login}>
+        <Container>
+          <Input
+            type='text'
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+          />
+          <Input
+            type='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </Container>
+        <Error error={error}>{error}</Error>
+        <button type='submit'>Login</button>
+      </form>
     </div>
   );
 });
