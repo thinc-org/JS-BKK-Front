9840a3488aac1fbcc10674e8db82cde6e322796c
new5558 <new17353@gmail.com>
2020-01-13 16:16:46 +0700

    fix: now.sh build error

--- pages/schedule/agenda.tsx
+++ pages/schedule/agenda.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
+
 interface Agenda {
-  hours:String;
-  minutes:String;
-  name:String;
+  hours: string;
+  minutes:string;
+  name: string;
 }
 function renderAgenda(tasks:Agenda[]) {
   const d = new Date();
-  const t = tasks.map((task) => {
-   
-   //   console.log(Date.parse("13 Jan 2020 "+task.hours+":"+task.minutes))
-      if(Date.parse("13/01/2020"+task.hours+":"+task.minutes)<Date.parse("13/01/2020"+d.getHours()+":"+d.getMinutes())){
-          return (
-              <div className="mt-40px ml-40px text-4xl">
-                  <div>
-                      {task.hours}.{task.minutes} - {task.name}
-                  </div>
-                  <hr className="mt-20px"></hr>
-              </div>
-          );
-      }else{
-          return (
-              <div className="mt-40px ml-40px text-4xl">
-                  <div className="text-gray-600">
-                      {task.hours}.{task.minutes} - {task.name}
-                  </div>
-                  <hr className="mt-20px"></hr>
-              </div>
-          );
-      }
-     
-  })
-    return t 
+  const t = tasks && tasks.map(task => {
+    //   console.log(Date.parse("13 Jan 2020 "+task.hours+":"+task.minutes))
+    if (
+      Date.parse(`13/01/2020${task.hours}:${task.minutes}`) <
+      Date.parse(`13/01/2020${d.getHours()}:${d.getMinutes()}`)
+    ) {
+      return (
+        <div key={task.name} className='mt-40px ml-40px text-4xl'>
+          <div>
+            {task.hours}.{task.minutes} - {task.name}
+          </div>
+          <hr className='mt-20px' />
+        </div>
+      );
+    }
+    return (
+      <div key={task.name} className='mt-40px ml-40px text-4xl'>
+        <div className='text-gray-600'>
+          {task.hours}.{task.minutes} - {task.name}
+        </div>
+        <hr className='mt-20px' />
+      </div>
+    );
+  });
+  return t 
 }
 export default renderAgenda;
