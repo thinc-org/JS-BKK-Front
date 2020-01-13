2d4e9f9221b7fd71b05c4829ece45d92cb185b68
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-01-13 12:39:34 +0700

    feat: login

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,39 +1,38 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
 import React, { useCallback, useContext, useState } from 'react';
 import { observer } from 'mobx-react-lite';
-// import { useRouter } from 'next/router';
-// import { rootContext } from '../_app';
+import { useRouter } from 'next/router';
+import { rootContext } from '../_app';
 
 interface ErrorProps {
   error: string | null;
 }
 
 const Home: React.FC = observer(() => {
-  // const { userStore } = useContext(rootContext);
+  const { userStore } = useContext(rootContext);
   // const [username, setUsername] = useState('');
   // const [password, setPassword] = useState('');
-  // const [error, setError] = useState<string | null>(null);
-  // const router = useRouter();
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
   const [ticketID, setTicketID] = useState();
 
 
-  // const login = useCallback(
-  //   e => {
-  //     e.preventDefault();
-  //     if (username === 'admin' && password === '123456') {
-  //       userStore.setToken('validtoken');
-  //       router.push('/user/profile');
-  //     } else {
-  //       setError('Username or password wrong');
-  //       setPassword('');
-  //       setUsername('');
-  //     }
-  //   },
-  //   [username, password]
-  // );
+  const login = useCallback(
+    e => {
+      e.preventDefault();
+      if (ticketID.length === 10) {
+        userStore.setToken('validtoken');
+        router.push('/user/profile');
+      } else {
+        setError('Username or password wrong');
+        
+      }
+    },
+    [ticketID]
+  );
 
   return (
-    <div className='flex flex-col items-center'>
+    <form onSubmit={login} className='flex flex-col items-center'>
       {/* <div className='bg-green-600 flex flex-row'>Login</div>
       <form onSubmit={login}>
         <input
@@ -50,6 +49,7 @@ const Home: React.FC = observer(() => {
         <button type='submit'>Login</button>
       </form> */}
 
+
       <img
         className='mt-16 border-black border-solid border w-40 h-40 rounded'
         src='https://images.pexels.com/photos/3496994/pexels-photo-3496994.jpeg?cs=srgb&dl=white-and-black-2020-with-confetti-3496994.jpg&fm=jpg'
@@ -63,16 +63,16 @@ const Home: React.FC = observer(() => {
 
       <p className='mt-10'>or Scan QR</p>
 
-      <label className="mt-3 py-2 px-8 bg-gray-800 text-white rounded">
+      <label className="mt-3 py-2 px-8 bg-gray-800 text-white rounded cursor-pointer">
         <span>Open Camera</span>
         <input className='hidden' type='file' />
       </label>
 
-      <button type="button" className="my-16 py-4 px-24 bg-gray-800 text-white rounded">
+      <button onClick={login} type="button" className="my-16 py-4 px-24 bg-gray-800 text-white rounded">
         Login
       </button>
-
-    </div>
+      {error}
+    </form>
   );
 });
 
