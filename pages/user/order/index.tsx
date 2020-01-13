406bd62e6c63e47fa20c8bdf03c00f6696080e20
KoFeeBriAN <coffeetk2017@gmail.com>
2020-01-13 16:55:21 +0700

    refactor(order): rename comp, add observer

--- pages/user/order/index.tsx
+++ pages/user/order/index.tsx
@@ -1,11 +1,12 @@
 import React, { useContext, useEffect, useState, useCallback } from 'react';
+import { observer } from 'mobx-react-lite';
 import { rootContext } from '../../_app';
 import { UserStore } from '../../../interfaces/interface.user';
 import useMockApi from '../../../commons/hooks/hook.mock-api';
 import { Order } from '../../../interfaces/interface.order';
 import OrderItem from './component.order-item';
 
-const UserProfile: React.FC = () => {
+const Orders: React.FC = observer(() => {
   const { userStore } = useContext<{ userStore: UserStore }>(rootContext);
   const [orders, setOrders] = useState<Order[] | undefined | null>();
   const mockApi = useMockApi('order');
@@ -18,11 +19,13 @@ const UserProfile: React.FC = () => {
     fetchData();
   }, []);
 
-  
-  const orderFood = useCallback((orderData: Order) => {
-    // eslint-disable-next-line no-console
-    console.log(orderData, 'order item')
-  }, [orders])
+  const orderFood = useCallback(
+    (orderData: Order) => {
+      // eslint-disable-next-line no-console
+      console.log(orderData, 'order item');
+    },
+    [orders]
+  );
 
   const OrderItems =
     orders &&
@@ -45,6 +48,6 @@ const UserProfile: React.FC = () => {
       </div>
     </div>
   );
-};
+});
 
-export default UserProfile;
+export default Orders;
