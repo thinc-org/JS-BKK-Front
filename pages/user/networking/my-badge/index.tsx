ec9ee98db798993da5b9afa70ef06f70dd748f21
KoFeeBriAN <coffeetk2017@gmail.com>
2020-01-13 12:48:28 +0700

    feat: networking 1 temp-name: `my-badge`

---
+++ pages/user/networking/my-badge/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Link from 'next/link';
+import Button from '../../../../commons/components/component.button';
+
+const MyBadge = () => {
+  return (
+    <div className='h-full my-24 px-12'>
+      <div id='content' className='flex flex-col justify-center items-center'>
+        <h3 className='mb-8'>You badge is</h3>
+        <div className='bg-red-300 w-40 h-40 flex justify-center items-center mb-12'>
+          BADGE
+        </div>
+        <p className='text-center'>
+          There are 7 total badges. Please talk to other people and scan their
+          QR code to obtain other 6 badges! The first 3 people will get the
+          reward!
+        </p>
+      </div>
+      <div className='cta fixed bottom-0 inset-x-0 flex justify-center'>
+        <Link href='/user/networking/profile'>
+          <a href="test">
+            <Button
+              className='mx-auto mb-16 px-20 py-4 bg-black text-white'
+              type='button'
+            >
+          ดำเนินการต่อ (Continue)
+            </Button>
+          </a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default MyBadge;
