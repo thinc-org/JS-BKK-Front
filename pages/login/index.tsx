631e3987b9c05f44f2f46ba1cb301576d70efa0c
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-01-13 12:19:41 +0700

    style: add style to buttons; rounded corner

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-// import React, { useCallback, useContext, useState } from 'react';
+import React, { useCallback, useContext, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 // import { useRouter } from 'next/router';
 // import { rootContext } from '../_app';
@@ -14,6 +14,8 @@ const Home: React.FC = observer(() => {
   // const [password, setPassword] = useState('');
   // const [error, setError] = useState<string | null>(null);
   // const router = useRouter();
+  const [ticketID, setTicketID] = useState();
+
 
   // const login = useCallback(
   //   e => {
@@ -49,24 +51,24 @@ const Home: React.FC = observer(() => {
       </form> */}
 
       <img
-        className='mt-16 border-black border-solid border w-40 h-40'
+        className='mt-16 border-black border-solid border w-40 h-40 rounded'
         src='https://images.pexels.com/photos/3496994/pexels-photo-3496994.jpeg?cs=srgb&dl=white-and-black-2020-with-confetti-3496994.jpg&fm=jpg'
         alt='logo'
       />
-      <p className='mt-8'>Please enter your Ticket ID</p>
+      <p className='mt-8'>Please enter your <span className="underline">Ticket ID</span></p>
 
       <form>
-        <input className='mt-5 border-black border-solid border' type='text' />
+        <input className='mt-5 border-black border-solid border rounded' type='text' value={ticketID} onChange={e => setTicketID(e.target.value)}/>
       </form>
 
       <p className='mt-10'>or Scan QR</p>
 
-      <label className="mt-3">
+      <label className="mt-3 py-2 px-8 bg-gray-800 text-white rounded">
         <span>Open Camera</span>
         <input className='hidden' type='file' />
       </label>
 
-      <button type="button" className="mt-16">
+      <button type="button" className="my-16 py-4 px-24 bg-gray-800 text-white rounded">
         Login
       </button>
 
