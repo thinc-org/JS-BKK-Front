cffb4297700cff7c74d760e63aaf34f7c26e0890
new5558 <new17353@gmail.com>
2020-01-13 15:06:31 +0700

    feat: networking/winner page

---
+++ pages/user/networking/winner/index.tsx
@@ -0,0 +1,39 @@
+import React, { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
+import { rootContext } from '../../../_app';
+import { UserStore } from '../../../../interfaces/interface.user';
+
+const UserProfile: React.FC = observer(() => {
+  const { userStore } = useContext<{ userStore: UserStore }>(rootContext);
+
+  return (
+    <>
+      <style jsx>
+        {`
+          .congrat-text {
+            max-width: 225px;
+          }
+        `}
+      </style>
+      <div className='h-screen'>
+        <div className='text-sm'>
+          Ordering as{' '}
+          <span className='font-bold'>{userStore.userInfo?.username}</span>
+        </div>
+        <div className='h-full text-center w-full flex flex-col items-center justify-center'>
+          <div className='congrat-text'>
+            <div className='mb-5 text-2xl font-bold text-center'>
+              Congratulations! You are the winner.
+            </div>
+            <div className='text-center'>
+              Please <span className='font-bold'>claim your reward</span>
+              at the information desk.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+});
+
+export default UserProfile;
