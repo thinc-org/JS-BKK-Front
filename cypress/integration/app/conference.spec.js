b68aeb43bf2e8e14e9896f904f3a3ca05b36ec67
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-18 18:44:39 +0700

    Add test prototype

--- cypress/integration/app/conference.spec.js
+++ cypress/integration/app/conference.spec.js
@@ -1,6 +1,16 @@
+/// <reference types="Cypress" />
+
 describe('Conference page', () => {
-  it('displays the latest announcement');
-  it('can display links in announcement');
+  it('displays the latest announcement', () => {
+    // Reset announcement text.
+    cy.visit('/prototype/conference.html');
+    cy.queryByText(/This is an announcement!/).should('exist');
+  });
+  it('can display links in announcement', () => {
+    // Reset announcement text.
+    cy.visit('/prototype/conference.html');
+    cy.queryByText('feedback').should('match', 'a[href]');
+  });
   it('has a tweet button');
   it('displays schedule');
   it('updates the announcement in real-time');
