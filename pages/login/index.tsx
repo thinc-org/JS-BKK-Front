08e88bfb6cbe25c735ab288c125413dec3f0df23
new5558 <new17353@gmail.com>
2020-01-18 17:52:52 +0700

    fix: all requested changes in #14

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -5,22 +5,19 @@ import { useRouter } from 'next/router';
 import dynamic from 'next/dynamic';
 import { rootContext } from '../_app';
 
-interface ErrorProps {
-  error: string | null;
-}
-
 const QrReader = dynamic(() => import('react-qr-reader'), {
-  ssr: false
+  ssr: false,
+  // eslint-disable-next-line react/display-name
+  loading: () => <p>Loading</p>
 });
 
 const Home: React.FC = observer(() => {
   const { userStore } = useContext(rootContext);
-  const [error, setError] = useState<string | null>(null);
-  const router = useRouter();
-  const [isOpen, setIsOpen] = useState(false);
-  const [qrReady, setQrReady] = useState<boolean>(false);
-
+  const [loginError, setLoginError] = useState<string | null>(null);
+  const [wrongInputFormat, setWrongInputFormat] = useState(false);
+  const [isScanningQR, setIsScanningQR] = useState<boolean>(false);
   const [ticketID, setTicketID] = useState<string>('');
+  const router = useRouter();
 
   const login = useCallback(
     e => {
@@ -29,7 +26,7 @@ const Home: React.FC = observer(() => {
         userStore.setToken('validtoken');
         router.push('/user/profile');
       } else {
-        setError('Username or password wrong');
+        setLoginError('Username or password wrong');
       }
     },
     [ticketID]
@@ -38,15 +35,21 @@ const Home: React.FC = observer(() => {
   const handleScan = (data: string | null) => {
     if (data) {
       setTicketID(data);
-      setQrReady(false);
+      setIsScanningQR(false);
     }
   };
 
   const handleError = (err: any) => {
-    console.error(err);
+    console.log(err);
   };
 
-  
+  const handleTicketIDChange = useCallback(e => {
+    const ticketIDInput = e.target.value;
+    const ticketLength = ticketIDInput.length;
+    const isInputMismatch = ticketLength < 10 || ticketLength > 10;
+    setTicketID(ticketIDInput);
+    setWrongInputFormat(isInputMismatch);
+  }, []);
 
   return (
     <form onSubmit={login} className='flex flex-col items-center'>
@@ -58,44 +61,31 @@ const Home: React.FC = observer(() => {
       <p className='mt-8'>
         Please enter your <span className='underline'>Ticket ID</span>
       </p>
-
       <input
         className='mt-5 border-black border-solid border rounded p-1'
         type='text'
         value={ticketID}
-        onChange={e => {
-          const loginError = e.target.value;
-          setTicketID(loginError);
-          if (loginError.length < 10 || loginError.length > 10) {
-            setIsOpen(true);
-          } else {
-            setIsOpen(false);
-          }
-        }}
+        onChange={handleTicketIDChange}
       />
-      <p className={`text-red-600 pt-2 ${!isOpen && 'hidden'}`}>
+      <p className={`text-red-600 pt-2 ${!wrongInputFormat && 'hidden'}`}>
         Please enter 10 digits
       </p>
-
       <p className='mt-10'>or Scan QR</p>
-
       <button
-        onClick={() => setQrReady(true)}
+        onClick={() => setIsScanningQR(true)}
         type='button'
         className='mt-3 py-2 px-8 bg-gray-800 text-white rounded'
       >
         Open Camera
       </button>
-
-      {qrReady && true ? (
+      {isScanningQR && (
         <QrReader
           className='w-40 h-40 mt-4 rounded'
           delay={300}
           onError={handleError}
           onScan={handleScan}
         />
-      ) : null}
-
+      )}
       <button
         onClick={login}
         type='button'
@@ -103,8 +93,7 @@ const Home: React.FC = observer(() => {
       >
         Login
       </button>
-
-      {error}
+      {loginError}
     </form>
   );
 });
