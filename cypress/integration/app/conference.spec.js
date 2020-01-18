4f0edc720a59ac69b4fa9c67ce8874d0242c003f
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-18 18:20:52 +0700

    Fix formatting

--- cypress/integration/app/conference.spec.js
+++ cypress/integration/app/conference.spec.js
@@ -1,7 +1,7 @@
 describe('Conference page', () => {
-  it('displays the latest announcement')
-  it('can display links in announcement')
-  it('has a tweet button')
-  it('displays schedule')
-  it('updates the announcement in real-time')
-})
+  it('displays the latest announcement');
+  it('can display links in announcement');
+  it('has a tweet button');
+  it('displays schedule');
+  it('updates the announcement in real-time');
+});
