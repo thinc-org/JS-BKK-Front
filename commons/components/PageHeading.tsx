beb6c010b43771ee6854447793c5e7473606504c
new5558 <new17353@gmail.com>
2020-01-22 14:47:44 +0700

    fix(heading): use observer hoc to handle heading component

--- commons/components/PageHeading.tsx
+++ commons/components/PageHeading.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { observer } from 'mobx-react-lite';
 import { RouteData } from '../../interfaces/interface.commons';
 import Button from './component.button';
 import { rootContext } from '../../pages/_app';
@@ -7,7 +8,7 @@ interface PropTypes {
   routeData: RouteData;
 }
 
-const PageHeading: React.FC<PropTypes> = ({ routeData }) => {
+const PageHeading: React.FC<PropTypes> = observer(({ routeData }) => {
   const { userStore } = useContext(rootContext);
   return (
     <nav className='flex flex-row items-center text-h text-white'>
@@ -19,6 +20,6 @@ const PageHeading: React.FC<PropTypes> = ({ routeData }) => {
       )}
     </nav>
   );
-};
+});
 
 export default PageHeading;
