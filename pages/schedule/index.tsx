8d285cd457cca70816bd0ab1480bb11e71d6df90
wasin <wasinph@outlook.com>
2020-01-23 11:05:30 +0700

    feat : add css for schedule

--- pages/schedule/index.tsx
+++ pages/schedule/index.tsx
@@ -2,26 +2,34 @@ import React from 'react';
 import {  useObserver } from "mobx-react";
 import { observer } from 'mobx-react-lite';
 import {ScheduleContext } from '../../commons/stores/schedule';
+import ScheduleBox from './schedulebox';
 
 const GetData = () =>{
   const store = React.createContext(ScheduleContext);
   return useObserver(() => <h1>{console.log(store)} </h1>);
 }
+
+
+
 const Schedule:React.FC= observer(() => {
   const store = React.useContext(ScheduleContext);
 
   return(<div>
     <div className="font-bold text-2xl">Conference</div>
-    <div className="w-8/12 h-1/12 mt-3">
-      <div className="mt-16">
-            Announcements 
-      </div>
-      <div className="mt-2">
-            Please Dont forget to make your food selection
+    <div className="flex justify-center">
+      <div className="border border-orange-400 rounded-lg py-45 px-120 bg-yellow-300 w-10/12 h-1/12 mt-3 ">
+        <div className="mt-2  ml-4">
+          <b>Announcements </b>
+        </div>
+        <div className="mt-2 ml-4">
+              Please Dont forget to make your food selection
+        </div>
       </div>
     </div>
-    <div className="mt-16 font-bold">Schedule</div>
+    <div className="mt-8 font-bold">Schedule</div>
     <GetData/>
+    <ScheduleBox/>
+   
 
   </div>);
 })
---
+++ pages/schedule/schedulebox.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const ScheduleBox =()=>{
+  return(
+    
+    <div>
+      <div className="border border-black bg-w-10/12 h-2/12">
+        <div className="mt-2">
+          18:30 P.M
+        </div>
+        <div>
+        Applying SOLID principle in JavaScript without Class and Object
+        </div>
+        <div className="flex justify-end mt-4">
+          By Chakrit Likitkhajorn
+        </div>
+      </div>
+    </div>
+  )
+}
+export default ScheduleBox;
