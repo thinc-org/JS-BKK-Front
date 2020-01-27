3b3849f28ca8e1462a075f6631dfd72b75d2d740
new5558 <new17353@gmail.com>
2020-01-27 15:06:16 +0700

    fix: screen blinking when transform page

--- commons/components/Nav.tsx
+++ commons/components/Nav.tsx
@@ -1,36 +1,39 @@
 import React from 'react';
-import Link from 'next/link';
+import Router from 'next/router';
 
 const Nav: React.FC<{}> = () => {
   return (
     <nav className='bg-white flex flex-row items-center justify-around text-xs'>
-      <Link href='/'>
-        <a
-          className='text-center flex flex-col items-center py-2 w-1/3'
-          href='/'
-        >
-          <div className='w-5 h-5 bg-grey' />
-          Conference
-        </a>
-      </Link>
-      <Link href='/user/order'>
-        <a
-          className='text-center flex flex-col items-center py-2 w-1/3'
-          href='/order'
-        >
-          <div className='w-5 h-5 bg-grey' />
-          Food
-        </a>
-      </Link>
-      <Link href='/user/networking/dashboard'>
-        <a
-          className='text-center flex flex-col items-center py-2 w-1/3'
-          href='/networking/dashboard'
-        >
-          <div className='w-5 h-5 bg-grey' />
-          Networking
-        </a>
-      </Link>
+      <div
+        role='button'
+        tabIndex={0}
+        onKeyPress={() => Router.push('/')}
+        onClick={() => Router.push('/')}
+        className='focus:outline-none text-center flex flex-col items-center py-2 w-1/3'
+      >
+        <div className='w-5 h-5 bg-grey' />
+        Conference
+      </div>
+      <div
+        role='button'
+        tabIndex={0}
+        onKeyPress={() => Router.push('/')}
+        onClick={() => Router.push('/user/order')}
+        className='focus:outline-none text-center flex flex-col items-center py-2 w-1/3'
+      >
+        <div className='w-5 h-5 bg-grey' />
+        Food
+      </div>
+      <div
+        role='button'
+        tabIndex={0}
+        onKeyPress={() => Router.push('/')}
+        onClick={() => Router.push('/user/networking/dashboard')}
+        className='focus:outline-none text-center flex flex-col items-center py-2 w-1/3'
+      >
+        <div className='w-5 h-5 bg-grey' />
+        Networking
+      </div>
     </nav>
   );
 };
