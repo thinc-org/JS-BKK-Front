e703a00963da923ecabb248f302fd9c8956f6578
Thai Pangsakulyanont <dtinth@spacet.me>
2020-01-28 23:49:38 +0700

    Add Tweet button

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -5,8 +5,22 @@ const Home: React.FC = () => {
   return (
     <div>
       <Announcements />
+      <TweetButton />
       <div className='text-red-600'>Home works</div>
     </div>
+  );
+};
+
+const TweetButton: React.FC = () => {
+  return (
+    <a
+      href='https://twitter.com/intent/tweet?hashtags=jsbangkok'
+      target='_blank'
+      rel='noopener noreferrer'
+      className='block mt-12 py-3 text-center font-bg bg-yellow-dark text-black rounded'
+    >
+      Tweet #jsbangkok
+    </a>
   );
 };
 
