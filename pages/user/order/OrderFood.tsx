51439d29c2ded906523227c8f8ed4039daabf27c
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-01-28 15:33:33 +0700

    add: gray colour
    modify tailwind

--- pages/user/order/OrderFood.tsx
+++ pages/user/order/OrderFood.tsx
@@ -18,19 +18,19 @@ const OrderFood: React.FC<Props> = ({ className }) => {
       <Card className='flex flex-col'>
         <h3 className='flex justify-center font-bold'>I am Thai Pasta</h3>
         <div className='my-4'>
-          <span>Food (อาหาร):</span>
+          <span className="text-gray-600">Food (อาหาร):</span>
           <p className='font-bold'>{data?.food}</p>
         </div>
         <div className='my-4 '>
-          <span>Spice level (ความเผ็ด):</span>
+          <span className="text-gray-600">Spice level (ความเผ็ด):</span>
           <p className='font-bold'>{data?.spice}</p>
         </div>
         <div className='my-4'>
-          <span>Drink (เครื่องดื่ม):</span>
+          <span className="text-gray-600">Drink (เครื่องดื่ม):</span>
           <p className='font-bold'>{data?.drink}</p>
         </div>
         <div className='my-4'>
-          <span>Dessert (ขนมหว่าน):</span>
+          <span className="text-gray-600">Dessert (ขนมหว่าน):</span>
           <p className='font-bold'>{data?.dessert}</p>
         </div>
         <Button type='button' className='bg-yellow-dark rounded p-2 m-4 text-xl'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,17 @@ module.exports = {
       black: '#000',
       white: '#fff',
       grey: '#C4C4C4',
+      gray: {
+        100: '#f7fafc',
+        200: '#edf2f7',
+        300: '#e2e8f0',
+        400: '#cbd5e0',
+        500: '#a0aec0',
+        600: '#718096',
+        700: '#4a5568',
+        800: '#2d3748',
+        900: '#1a202c'
+      },
       bkk: {
         white: '#F5F1E9',
         blue: '#164194',
