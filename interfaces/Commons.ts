4b3cef616d4ec87b1d6ffb8f6fcb837e31f60f24
Thai on Cloud9 <dtinth@spacet.me>
2020-01-30 16:46:00 +0000

    Add JSDoc for FetchResult

--- interfaces/Commons.ts
+++ interfaces/Commons.ts
@@ -27,12 +27,19 @@ export interface FetchedResult<T> {
   status: 'completed';
 }
 
+/**
+ * Checks if fetching is completed.
+ * If yes, then fetchResult.data will not be undefined.
+ */
 export function isFetchingCompleted<T>(
   f: FetchResult<T>
 ): f is FetchedResult<T> {
   return f.status === 'completed';
 }
 
+/**
+ * Checks if there is some fetching ongoing.
+ */
 export function isFetching<T>(f: FetchResult<T>): boolean {
   return f.status === 'loading';
 }
