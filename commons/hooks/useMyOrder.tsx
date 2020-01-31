3a7ae30ea53ccc15dd8c28c915172e5679762387
Thai on Cloud9 <dtinth@spacet.me>
2020-01-31 18:23:39 +0000

    Refactor useMyOrder use `useFirestoreSnapshot`

--- commons/hooks/useMyOrder.tsx
+++ commons/hooks/useMyOrder.tsx
@@ -1,42 +1,29 @@
+import { useCallback } from 'react';
 import {
   FetchResult,
   isFetchingCompleted,
   isFetchingFailed
 } from '../../interfaces/Commons';
 import { MyOrder } from '../../interfaces/Orders';
-import { useFirebase, FirestoreSnapshot } from '../firebase';
-import { useEffect, useState } from 'react';
+import { FirebaseModule, useFirestoreSnapshot } from '../firebase';
 
 const useMyOrder = (): FetchResult<MyOrder | null> => {
-  const firebaseFetchResult = useFirebase();
-
-  const [snapshot, setSnapshot] = useState<FirestoreSnapshot | null>(null);
-  const [error, setError] = useState<Error | null>(null);
-
-  useEffect(() => {
-    if (!isFetchingCompleted(firebaseFetchResult)) {
-      return () => {};
-    }
-    const firebase = firebaseFetchResult.data;
-    return firebase
-      .getEnvDoc()
-      .collection('foodChoices')
-      .doc(firebase.auth().currentUser!.uid)
-      .onSnapshot(setSnapshot, setError);
-  }, [firebaseFetchResult]);
-
-  if (isFetchingFailed(firebaseFetchResult)) {
-    return { status: 'error', error: firebaseFetchResult.error };
+  const getDocument = useCallback(
+    (firebase: FirebaseModule) =>
+      firebase
+        .getEnvDoc()
+        .collection('foodChoices')
+        .doc(firebase.auth().currentUser!.uid),
+    []
+  );
+  const snapshotFetchResult = useFirestoreSnapshot(getDocument);
+  if (isFetchingFailed(snapshotFetchResult)) {
+    return { status: 'error', error: snapshotFetchResult.error };
   }
-  if (!isFetchingCompleted(firebaseFetchResult)) {
+  if (!isFetchingCompleted(snapshotFetchResult)) {
     return { status: 'loading' };
   }
-  if (error) {
-    return { status: 'error', error: error };
-  }
-  if (!snapshot) {
-    return { status: 'loading' };
-  }
+  const snapshot = snapshotFetchResult.data;
   if (!snapshot.exists) {
     return { status: 'completed', data: null };
   }
