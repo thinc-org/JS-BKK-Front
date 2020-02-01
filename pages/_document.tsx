5ccb1ee67612e85f5e24511d89c76cfd888abb28
Thai on Cloud9 <dtinth@spacet.me>
2020-02-01 18:34:54 +0000

    Better make this page English

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,7 +14,7 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang='th'>
+      <Html lang='en'>
         <Head>
           <meta
             name='msapplication-config'
