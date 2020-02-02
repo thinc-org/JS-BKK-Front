cf87017b8740994b95f294f69f0e206347d58cc6
Thai on Cloud9 <dtinth@spacet.me>
2020-02-02 17:32:55 +0000

    Update restaurant availability in real time

--- commons/firebase/index.ts
+++ commons/firebase/index.ts
@@ -17,6 +17,8 @@ export type FirebaseModule = typeof import('../../commons/firebase/firebase');
 export type User = import('firebase').User;
 export type FirestoreReference = import('firebase').firestore.DocumentReference;
 export type FirestoreSnapshot = import('firebase').firestore.DocumentSnapshot;
+export type RealtimeDatabaseReference = import('firebase').database.Reference;
+export type RealtimeDatabaseSnapshot = import('firebase').database.DataSnapshot;
 
 export function useFirebase() {
   return useFetcher(getFirebase);
@@ -36,6 +38,43 @@ export function useFirestoreSnapshot(
     }
     const firebase = firebaseFetchResult.data;
     return getRef(firebase).onSnapshot(setSnapshot, setError);
+  }, [firebaseFetchResult, getRef]);
+
+  if (isFetchingFailed(firebaseFetchResult)) {
+    return { status: 'error', error: firebaseFetchResult.error };
+  }
+  if (!isFetchingCompleted(firebaseFetchResult)) {
+    return { status: 'loading' };
+  }
+  if (error) {
+    return { status: 'error', error: error };
+  }
+  if (!snapshot) {
+    return { status: 'loading' };
+  }
+  return { status: 'completed', data: snapshot };
+}
+
+export function useRealtimeDatabaseSnapshot(
+  getRef: (firebase: FirebaseModule) => RealtimeDatabaseReference
+): FetchResult<RealtimeDatabaseSnapshot> {
+  const firebaseFetchResult = useFirebase();
+
+  const [snapshot, setSnapshot] = useState<RealtimeDatabaseSnapshot | null>(
+    null
+  );
+  const [error, setError] = useState<Error | null>(null);
+
+  useEffect(() => {
+    if (!isFetchingCompleted(firebaseFetchResult)) {
+      return () => {};
+    }
+    const firebase = firebaseFetchResult.data;
+    const ref = getRef(firebase);
+    ref.on('value', setSnapshot, setError);
+    return () => {
+      ref.off('value', setSnapshot);
+    };
   }, [firebaseFetchResult, getRef]);
 
   if (isFetchingFailed(firebaseFetchResult)) {
