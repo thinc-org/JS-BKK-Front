bdb5c5e64d48f259b4db87ab8d25d6f4b1410baa
Thai on Cloud9 <dtinth@spacet.me>
2020-02-04 17:03:07 +0000

    Refactor parts of Countdown

--- components/order-food/CountDown.tsx
+++ components/order-food/CountDown.tsx
@@ -8,7 +8,7 @@ function padZero(n: string, width: number) {
   return n.length >= width ? n : new Array(width - n.length + 1).join('0') + n;
 }
 
-const Countdown: React.FC<Props> = ({ due }) => {
+export function useCurrentTime() {
   const [time, setTime] = useState<number | null>(null);
 
   useEffect(() => {
@@ -19,22 +19,29 @@ const Countdown: React.FC<Props> = ({ due }) => {
     return () => clearInterval(interval);
   }, []);
 
-  const timeLeft = useMemo(() => {
-    if (time === null) return '…';
-    const difference = Math.floor((due - time) / 1000);
-    if (difference < 0) return '00:00:00';
-    const seconds = difference % 60;
-    const minutes = Math.floor(difference / 60) % 60;
-    const hours = Math.floor(difference / 3600) % 24;
-    const days = Math.floor(difference / 86400);
-    const parts = [
-      ...(days > 0 ? [days] : []),
-      padZero(`${hours}`, 2),
-      padZero(`${minutes}`, 2),
-      padZero(`${seconds}`, 2)
-    ];
-    return parts.join(':');
-  }, [time, due]);
+  return time;
+}
+
+export const formatTimeLeft = (time: number | null, due: number) => {
+  if (time === null) return '…';
+  const difference = Math.floor((due - time) / 1000);
+  if (difference < 0) return '00:00:00';
+  const seconds = difference % 60;
+  const minutes = Math.floor(difference / 60) % 60;
+  const hours = Math.floor(difference / 3600) % 24;
+  const days = Math.floor(difference / 86400);
+  const parts = [
+    ...(days > 0 ? [days] : []),
+    padZero(`${hours}`, 2),
+    padZero(`${minutes}`, 2),
+    padZero(`${seconds}`, 2)
+  ];
+  return parts.join(':');
+};
+
+const Countdown: React.FC<Props> = ({ due }) => {
+  const time = useCurrentTime();
+  const timeLeft = useMemo(() => formatTimeLeft(time, due), [time, due]);
 
   return <>{timeLeft}</>;
 };
