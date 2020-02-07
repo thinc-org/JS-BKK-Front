4a2ce96d57ca5ba0829a3b10ad5e0a8a0f7af19a
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-02-07 22:08:02 +0700

    feat: my-badge

--- pages/user/networking/my-badge/index.tsx
+++ pages/user/networking/my-badge/index.tsx
@@ -6,34 +6,54 @@ import Button from '../../../../commons/components/Button';
 import { RootStore } from '../../../../interfaces/Commons';
 import rootContext from '../../../../commons/context.root';
 import { withRequiredAuthentication } from '../../../../components/authentication';
+import Card from '../../../../commons/components/Card';
 
 const MyBadge: React.FC = observer(() => {
-  const { userStore } = useContext<RootStore>(rootContext);
+  // const { userStore } = useContext<RootStore>(rootContext);
   return (
-    <div className='h-full px-12'>
-      <div id='content' className='flex flex-col justify-center items-center'>
-        <h3>You badge is</h3>
-        <div className='bg-red-300 w-40 h-40 flex justify-center items-center m-6'>
-          {userStore.userInfo?.currentBadge.type}
-        </div>
-        <p className='text-center'>
+    // <div className='h-full px-12'>
+    //   <div id='content' className='flex flex-col justify-center items-center'>
+    //     <h3>You badge is</h3>
+    //     <div className='bg-red-300 w-40 h-40 flex justify-center items-center m-6'>
+    //       {userStore.userInfo?.currentBadge.type}
+    //     </div>
+    //     <p className='text-center'>
+    //       There are 7 total badges. Please talk to other people and scan their
+    //       QR code to obtain other 6 badges! The first 3 people will get the
+    //       reward!
+    //     </p>
+    //   </div>
+    //   <div className='flex justify-center'>
+    //     <Link href='/user/networking/dashboard'>
+    //       <a href='test'>
+    //         <Button
+    //           className='mx-auto mb-16 px-20 py-4 bg-black text-white'
+    //           type='button'
+    //         >
+    //           Continue
+    //         </Button>
+    //       </a>
+    //     </Link>
+    //   </div>
+    // </div>
+    <div>
+      <Card className='flex flex-col items-center m-4 text-center p-5'>
+        <p className="">Your badge is</p>
+        <img className='h-32' src='/icons/myBadge.svg' alt='My Badge' />
+        <p>
           There are 7 total badges. Please talk to other people and scan their
           QR code to obtain other 6 badges! The first 3 people will get the
           reward!
         </p>
-      </div>
-      <div className='flex justify-center'>
-        <Link href='/user/networking/dashboard'>
-          <a href='test'>
-            <Button
-              className='mx-auto mb-16 px-20 py-4 bg-black text-white'
-              type='button'
-            >
-              Continue
-            </Button>
-          </a>
-        </Link>
-      </div>
+        <img className="h-32" src='/icons/scanQR.svg' alt="Scan QRCode instruction "/>
+        <Button
+          type='button'
+          className='bg-yellow-dark w-full rounded px-4 py-3 m-4 text-lg'
+          // onClick={}
+        >
+          Submit
+        </Button>
+      </Card>
     </div>
   );
 });
