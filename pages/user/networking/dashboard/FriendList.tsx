f3be0abdcae7b68b64b9557d51002aa74d60b265
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-02-08 02:51:25 +0700

    friend list new design

--- pages/user/networking/dashboard/FriendList.tsx
+++ pages/user/networking/dashboard/FriendList.tsx
@@ -9,11 +9,11 @@ interface Props {
 
 const FriendList: React.FC<Props> = ({ networks }) => {
   return networks
-    ? networks.map((network: { badge: number; name: string }) => {
+    ? networks.map((network: { badge: number; name: string }, index: number) => {
         return (
-          <div key={network.badge} className='flex flex-row items-center'>
+          <div key={network.badge} className={(index < networks.length - 1) ? 'flex flex-row items-center border-b border-gray-400 p-3' : 'flex flex-row items-center p-3' }>
             <BadgeList id={network.badge} className='h-8 mr-4' />
-            <p>{network.name}</p>
+            <p>{network.name} {networks.length}</p>
           </div>
         );
       })
