2692b1fdf7edcd35e0e5449eeca7c9207bb76ace
Boom PT <37561295+boompikachu@users.noreply.github.com>
2020-02-07 22:12:34 +0700

    add padding

--- pages/user/networking/my-badge/index.tsx
+++ pages/user/networking/my-badge/index.tsx
@@ -38,14 +38,14 @@ const MyBadge: React.FC = observer(() => {
     // </div>
     <div>
       <Card className='flex flex-col items-center m-4 text-center p-5'>
-        <p className="">Your badge is</p>
-        <img className='h-32' src='/icons/myBadge.svg' alt='My Badge' />
-        <p>
+        <p className="mb-8">Your badge is</p>
+        <img className='h-32 mb-4' src='/icons/myBadge.svg' alt='My Badge' />
+        <p className="mb-6">
           There are 7 total badges. Please talk to other people and scan their
           QR code to obtain other 6 badges! The first 3 people will get the
           reward!
         </p>
-        <img className="h-32" src='/icons/scanQR.svg' alt="Scan QRCode instruction "/>
+        <img className="h-32 mb-5" src='/icons/scanQR.svg' alt="Scan QRCode instruction "/>
         <Button
           type='button'
           className='bg-yellow-dark w-full rounded px-4 py-3 m-4 text-lg'
